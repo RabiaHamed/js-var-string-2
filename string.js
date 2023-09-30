676c32f09be7ba0936d6e1c6f593825a3fe5edb1
Rabia Hameed <nrsuriya093@gmail.com>
2023-09-30 00:08:08 -0700

    first commit

---
+++ string.js
@@ -0,0 +1,9 @@
+var age="I am 15 years old";
+alert(age);
+
+var tittle="You have visited this site 14 times";
+alert(tittle);
+
+var display="My birth year is 1990 Data type of my ddeclared variable is number";
+
+var window="John Doe orderd 5 T-shirt (s) on XYZ Clothing store";
